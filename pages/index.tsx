aaf90edb7e1c71aed6a79013e14a15580939653e
Jerry <jerry.guerrero95@gmail.com>
2022-10-30 02:06:44 -0500

    Simple page with card in the middle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <body className="h-screen bg-gradient-to-br from-gray-700 to-gray-800">
       <Head>
         <title>Jerry Guerrero</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* div that pads the screen on all sides */}
+      <div className="h-full px-16 pb-16 pt-32">
+        {/* div that contains the content */}
+        <div className="h-full bg-gray-900 rounded-3xl shadow-2xl"></div>
+      </div>
+    </body>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Home
+export default Home;
