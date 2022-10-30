67280db1da0c24c3e287b384f839fe5e1f4de2ef
Jerry <jerry.guerrero95@gmail.com>
2022-10-30 04:05:58 -0500

    prevent hydration error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // This is done to prevent a hydration error
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
