628ba525f1d08891fdd5631a7f6640069840a210
Jerry <jerry.guerrero95@gmail.com>
2022-10-31 03:16:53 -0500

    Add bottom border to demo data if not the last one

--- components/ProfileDemoInfo.tsx
+++ components/ProfileDemoInfo.tsx
@@ -6,21 +6,28 @@ export default function ProfileDemoInfo() {
   return (
     <div className="m-8 p-8 bg-gray-800 rounded-2xl flex flex-col gap-4">
       {/* Loop through demoInfoData */}
+      {demoInfoData.map((demoInfo, i, arr) => (
+        <>
           <div className="flex flex-row gap-2" key={i}>
-          {/* The icon on the row */}
-          <div className="flex-col">
-            <div className="rounded-lg bg-gray-900 p-2">
-              <demoInfo.Icon size={32} />
+            {/* The icon on the row */}
+            <div className="flex-col">
+              <div className="rounded-lg bg-gray-900 p-2">
+                <demoInfo.Icon size={32} />
+              </div>
+            </div>
+            {/* The text on the row */}
+            <div className="flex-col">
+              <div className="text-md text-gray-400 my-[-3px]">
+                {demoInfo.subText}
+              </div>
+              <div className="text-white text-md">{demoInfo.text}</div>
             </div>
           </div>
-          {/* The text on the row */}
-          <div className="flex-col">
-            <div className="text-md text-gray-400 my-[-3px]">
-              {demoInfo.subText}
-            </div>
-            <div className="text-white text-md">{demoInfo.text}</div>
-          </div>
-        </div>
+          {/* Bottom border, only if not the last element*/}
+          {i + 1 !== arr.length && (
+            <div className="border-b border-gray-700 my-[-0.3rem]" />
+          )}
+        </>
       ))}
     </div>
   );
