bf7674493403b8eda38961caaeb0be6ec9b5b684
Jerry <jerry.guerrero95@gmail.com>
2022-10-30 20:07:21 -0500

    darken bg of social links, hover turns them blue

--- components/ProfileSocialLink.tsx
+++ components/ProfileSocialLink.tsx
@@ -12,7 +12,7 @@ export default function SocialLink({ href, Icon, color }: SocialLinkProps) {
   return (
     <a
       href={href}
-      className="bg-gray-700 rounded-md p-2 transform transition duration-500 hover:scale-125 hover:bg-gray-800"
+      className="bg-gray-800 rounded-md p-2 transform transition duration-500 hover:scale-125 hover:bg-blue-900"
     >
       <Icon size={32} style={{ color: color }} />
     </a>
