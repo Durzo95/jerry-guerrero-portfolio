bfce2072107783d9e72c2bba958b4a9a383dfca1
Jerry <jerry.guerrero95@gmail.com>
2022-10-31 16:59:32 -0500

    Update title

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -10,7 +10,7 @@ export default function Profile() {
       <h1 className="text-4xl font-bold text-center">Jerry Guerrero</h1>
       {/* The title */}
       <h2 className="text-2xl text-center text-gray-400 mt-4">
-        Application Manager
+        Data Application Manager
       </h2>
       {/* The social media links */}
       <ProfileSocialLink />
