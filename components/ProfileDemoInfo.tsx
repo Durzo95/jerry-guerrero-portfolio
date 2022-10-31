32de256123fa39dd9bd38c890469e17a3dbb954b
Jerry <jerry.guerrero95@gmail.com>
2022-10-31 01:42:56 -0500

    Finished a line on the demographic info

--- components/ProfileDemoInfo.tsx
+++ components/ProfileDemoInfo.tsx
@@ -4,11 +4,17 @@ import { IoSchool } from "react-icons/io5";
 export default function ProfileDemoInfo() {
   return (
     <div className="m-8 p-8 bg-gray-800 rounded-2xl">
-      <div className="flex flex-row">
-        <div className="flex flex-col">
-          <h3 className="rounded-lg bg-gray-900 p-2">
+      <div className="flex flex-row gap-2">
+        {/* The icon on the row */}
+        <div className="flex-col">
+          <div className="rounded-lg bg-gray-900 p-2">
             <IoSchool size={32} />
-          </h3>
+          </div>
+        </div>
+        {/* The text on the row */}
+        <div className="flex-col">
+          <div className="text-md font-bold text-gray-400">Education</div>
+          <div className="text-white text-lg">Computer Science, Bachelor</div>
         </div>
       </div>
     </div>
