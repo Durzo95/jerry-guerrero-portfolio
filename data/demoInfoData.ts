b451bbb4bab57eb7ecec4b9abb2d56f4aef5cbce
Jerry <jerry.guerrero95@gmail.com>
2022-10-31 17:01:06 -0500

    Add Location and change Io school Icon to MD

--- data/demoInfoData.ts
+++ data/demoInfoData.ts
@@ -1,24 +1,28 @@
 // an object that holds the social media links
-import { IoSchool } from "react-icons/io5";
-import { MdComputer, MdEmail } from "react-icons/md";
+import { MdComputer, MdEmail, MdLocationPin, MdSchool } from "react-icons/md";
 import { IconType } from "react-icons";
 
 // the social media links
 const demoInfoData: Array<{ Icon: IconType; text: string; subText: string }> = [
   {
-    Icon: IoSchool,
-    text: "Computer Science, Bachelor 2020",
-    subText: "Education",
+    Icon: MdComputer,
+    text: "Data Services - IDEA Public Schools",
+    subText: "Current Employment",
   },
   {
-    Icon: MdComputer,
-    text: "Four Years of Experience",
-    subText: "Work Experience",
+    Icon: MdSchool,
+    text: "Computer Science, Bachelor 2020",
+    subText: "Education",
   },
   {
     Icon: MdEmail,
     text: "Jerry.Guerrero95@gmail.com",
     subText: "Email",
+  },
+  {
+    Icon: MdLocationPin,
+    text: "McAllen, Texas",
+    subText: "Location",
   },
 ];
 
