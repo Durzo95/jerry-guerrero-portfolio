8d56152a736c8d47a9eb10be9eb5e7a16738f749
Jerry <jerry.guerrero95@gmail.com>
2022-11-01 23:43:47 -0500

    Finished about me section

--- components/MainBody.tsx
+++ components/MainBody.tsx
@@ -2,27 +2,31 @@ import React from "react";
 import MainBodyTitle from "./MainBodyTitle";
 export default function MainBody() {
   return (
-    <div className="h-max w-2/3 bg-gray-900 rounded-2xl shadow-2xl py-8 px-6">
-      {/* About Me Title */}
-      <MainBodyTitle title="About Me" />
-      <div className="text-gray-400">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo mollitia,
-        blanditiis saepe inventore animi maiores sunt, eius cupiditate quo
-        nesciunt nam assumenda similique minima iste voluptate possimus deserunt
-        ipsum culpa? Nihil, dolorem beatae. Id, deleniti, omnis a ullam labore
-        pariatur numquam unde fugit modi quo explicabo natus! Quae laborum
-        doloremque quis. Accusantium culpa eligendi totam dolor officiis nihil
-        quo asperiores. Praesentium ipsa quo aperiam velit, esse rem, tempore
-        corporis delectus odit fugit harum doloribus nobis animi provident minus
-        odio amet pariatur cumque! Harum neque odit corporis atque itaque!
-        Nesciunt, harum? Cum officia rerum voluptatum. Qui, laborum culpa. Eius
-        voluptatem iure ex voluptate illum dolores, aliquam perspiciatis quasi!
-        Dolor placeat nisi non, fugit quidem perspiciatis architecto
-        reprehenderit aliquam nostrum error odio! Quidem vel repudiandae
-        placeat? Iusto, unde aliquam earum expedita incidunt nisi obcaecati
-        ipsam maiores quibusdam aut, quas animi ab modi nesciunt praesentium.
-        Soluta aperiam maxime ut omnis enim earum consequatur!
+    <>
+      <div className="h-max w-2/3 bg-gray-900 rounded-2xl shadow-2xl py-8 px-6">
+        {/* About Me Title */}
+        <MainBodyTitle title="About Me" />
+        <div className="text-gray-400 mb-8">
+          I am a Computer Science graduate working as the Data Applications
+          Manager for IDEA Public Schools. Currently managing the Applications
+          team to administrate 4 Student Information Systems (SIS) in 4
+          different states with a grand total of over 9,000 school admins and
+          80,000 students.
+          <div className="mt-4" /> {/* Line Break */}
+          My technical skills involve working with Python, SQL, and JavaScript.
+          With these tools I have solved and automated complex processes for the
+          organization. This includes the imports of student data from the
+          student registration platform to the SIS systems, the automation of
+          our staff accounts, and boundary checks to make sure we are only
+          enrolling students within our district boundary.
+          <div className="mt-4" /> {/* Line Break */}
+          For soft skills, I have experience in managing our applications team
+          through clear communication of our goals, processes, and training. We
+          have not only been able to meet our goals but also exceed them every
+          year.
+        </div>
+        <MainBodyTitle title="What I do" />
       </div>
-    </div>
+    </>
   );
 }
