b52cd0138da3e77edf6eedb2fd5c4e947670cd74
Jerry <jerry.guerrero95@gmail.com>
2022-11-01 23:29:03 -0500

    resizing main components

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,13 @@ import MainBody from "../components/MainBody";
 
 const Home: NextPage = () => {
   return (
-    <body className="min-h-screen bg-gray-800 py-32 px-16">
+    <body className="min-h-screen bg-gray-800 py-32 px-10">
       <Head>
         <title>Jerry Guerrero</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Holding all the content, set the flex row */}
-      <div className="flex flex-row gap-12">
+      <div className="flex flex-row gap-8">
         {/* Left side of the page, the profile */}
         <Profile />
         {/* Right side of the page, the content */}
