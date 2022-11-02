22af5aa9f2a852b161a62ec13bfce91c690c9065
Jerry <jerry.guerrero95@gmail.com>
2022-11-01 23:25:10 -0500

    Moved profile title to it's own component

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ProfileTitle from "./ProfileTitle";
 import ProfileSocialLink from "./ProfileSocialLink";
 import ProfileDemoInfo from "./ProfileDemoInfo";
 import DownloadCV from "./DownloadCV";
@@ -6,12 +7,8 @@ import DownloadCV from "./DownloadCV";
 export default function Profile() {
   return (
     <div className="w-1/3 max-w-md min-w-[28rem] h-max p-8 bg-gray-900 rounded-2xl shadow-2xl text-white">
-      {/* The name */}
-      <h1 className="text-4xl font-bold text-center">Jerry Guerrero</h1>
-      {/* The title */}
-      <h2 className="text-2xl text-center text-gray-400 mt-4">
-        Data Application Manager
-      </h2>
+      {/* Profile Title */}
+      <ProfileTitle />
       {/* The social media links */}
       <ProfileSocialLink />
       {/* The Profile demographic overview */}
---
+++ components/ProfileTitle.tsx
@@ -0,0 +1,12 @@
+export default function ProfileTitle() {
+  return (
+    <>
+      {/* The name */}
+      <h1 className="text-4xl font-bold text-center">Jerry Guerrero</h1>
+      {/* The title */}
+      <h2 className="text-2xl text-center text-gray-400 mt-4">
+        Data Application Manager
+      </h2>
+    </>
+  );
+}
