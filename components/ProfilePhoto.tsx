231ddd94c07242e9fdfbf1abc699299e9aa8ad25
Jerry <jerry.guerrero95@gmail.com>
2022-11-03 02:51:08 -0500

    Squashed commit of the following:
    commit 7b1c9c3639436e0f567a0eded228e548fc18244d
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 02:48:37 2022 -0500
    
        Remove email due to redundancy
    
    commit 8eeb554df28c7d4a1a9575ff1fb72f75080b743f
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 02:46:51 2022 -0500
    
        Blur photo on loading
    
    commit 9d1c4dba98a1d2f86526b9a04a16d772fc0ebd45
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 02:46:46 2022 -0500
    
        Fix Head on div tag error
    
    commit cf9682b3a12930fb79f92b7ce836c1331442f168
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 02:36:08 2022 -0500
    
        Keep background color even on mobile scroll
    
    commit e4c22ca36ee0fb0d9fc672b3cf626f14bede4a59
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 02:32:59 2022 -0500
    
        Fix padding on mobile
    
    commit 76f9454b295f04edc38f6aed86acbcfc7928ba22
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 02:25:28 2022 -0500
    
        Removed overflow of profile picture to keep it simple
    
    commit 33f32c2a737b0667b3e65d413c840e5a81c7c3cc
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 02:17:51 2022 -0500
    
        Width on mobile
    
    commit a9bb0d52cf40374a369b0bfa53ccd22eff48b357
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 02:16:12 2022 -0500
    
        Profile picture set on resize
    
    commit eecf6963cd23bd3e2236c431ce50838e9cc08800
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 02:09:10 2022 -0500
    
        Flex to column when on smaller screens
    
    commit c555035a65e0e8f95cdd4b2d27313364f96e52ea
    Author: Jerry <jerry.guerrero95@gmail.com>
    Date:   Thu Nov 3 00:58:11 2022 -0500
    
        Beginning to setup mobile view

--- components/ProfilePhoto.tsx
+++ components/ProfilePhoto.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export default function ProfilePhoto() {
   return (
-    <div className="absolute left-[50%] transform translate-x-[-50%] mt-[-140px]">
+    <div className="flex justify-center mb-4">
       <Image
         src="/Headshot.png"
         alt="Jerry Guerrero"
         width={300}
         height={300}
         className="bg-blue-800 rounded-2xl shadow-2xl"
+        placeholder="blur"
+        blurDataURL="/Headshot.png"
       />
     </div>
   );
