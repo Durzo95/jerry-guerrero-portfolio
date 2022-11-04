bf30374fb52314a1a30dba7a6dd484b25e509a3f
Jerry <jerry.guerrero95@gmail.com>
2022-11-03 22:10:11 -0500

    Conver to grid instead of Flex

--- components/MainBody.tsx
+++ components/MainBody.tsx
@@ -5,7 +5,7 @@ import MainBodyWhatIDo from "./MainBodyWhatIDo";
 export default function MainBody() {
   return (
     <>
-      <div className="h-max w-full lg:w-2/3 bg-gray-900 rounded-2xl shadow-2xl py-8 px-6">
+      <div className="h-max w-full bg-gray-900 sm:rounded-2xl shadow-2xl py-8 px-6">
         {/* About Me */}
         <MainBodyAboutMe />
         {/* What I do */}
